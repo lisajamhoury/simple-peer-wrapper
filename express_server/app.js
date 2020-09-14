1d2410a3bac32cfe5aa1c71111291ca11ae99641
Adam Krebs <akre54@users.noreply.github.com>
2020-09-14 14:33:43 -0400

    Fix error handler
    Was getting `Error: Failed to lookup view "errors" in views directory "/Users/adam/Projects/WebRTC-Simple-Peer-Examples/express_server/views"` when the server threw an error. There is a template `error.jade` but no `errors.jade`

--- express_server/app.js
+++ express_server/app.js
@@ -52,7 +52,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('errors');
+  res.render('error');
 });
 
 // for automatic server reload
